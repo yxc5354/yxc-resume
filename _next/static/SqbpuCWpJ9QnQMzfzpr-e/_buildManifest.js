d342a46ef38d55a4a161e242423e7b1274a9afee
yxc <565294508@qq.com>
2022-03-25 01:04:20 +0800

    fix:更新

--- _next/static/SqbpuCWpJ9QnQMzfzpr-e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6f136973dad3b9e5.css","static/chunks/pages/index-a1888822dd21f7fc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
