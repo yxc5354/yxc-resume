9322fb8df15c8b7ca99c9fad69f29cc3fefe8ef3
yxc <565294508@qq.com>
2022-04-03 01:27:27 +0800

    feat: 更新

---
+++ _next/static/_-UXsk-gJCjQIRaeJ68Xc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/44527975d07ed287.css","static/chunks/pages/index-59f92907c4beadb5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
