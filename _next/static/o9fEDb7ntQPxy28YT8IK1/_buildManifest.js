e5cfe82b4c702ff149cc5a112c3183c569215229
yxc <565294508@qq.com>
2022-04-10 23:19:17 +0800

    feat:更新

---
+++ _next/static/o9fEDb7ntQPxy28YT8IK1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd7329d6b396c60b.css","static/chunks/pages/index-22b8ae98b6f25b9f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/z1R-jarnOcFoTtTSB-ZuG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4fb58d357bb954ce.css","static/chunks/pages/index-088f57d4d8c693cb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
