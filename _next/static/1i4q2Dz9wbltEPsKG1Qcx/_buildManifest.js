824715c9a8cfe32303cd6234c54aba8e3f0e0b1b
yxc <565294508@qq.com>
2023-03-13 14:18:36 +0800

    fix:更新

--- _next/static/1i4q2Dz9wbltEPsKG1Qcx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8f4f54c2a8b327a9.css","static/chunks/pages/index-d2cfcb43d0afa56b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/TF4VnF_dRM0gto88wHJE6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8f4f54c2a8b327a9.css","static/chunks/pages/index-e8ec2235feeb9eba.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
