d70453de4ae6985705c30d4537c79d0de72d91ca
yxc <565294508@qq.com>
2023-03-13 12:36:11 +0800

    feat: 更新

--- _next/static/kKpa8GJCiqs0YhALubZ_k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd7329d6b396c60b.css","static/chunks/pages/index-10de48b608199af2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
